a50daacc463f7701f058662c404d3e0d38b92af5
Anurag <anuragtivari850@gmail.com>
2023-12-30 16:41:25 +0530

    handle no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noimage from '../assets/3592838_average image_image_images_landscape_photography_icon.webp'
+
 const getCroppedImageUrl = (url: string) =>{
-    if(!url) return "";
+    if(!url) return noimage;
     const target = 'media/';
     const index = url.indexOf(target)+target.length;
    return  url.slice(0,index) + 'crop/600/400/' + url.slice(index);
